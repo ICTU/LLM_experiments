6e61f691eb026115d3eabe52336c6c98f38e1aa1
Frank Niessink <frank@niessink.com>
2024-02-16 10:05:49 +0100

    Summarize files in a directory separately from the subdirectories. (#6)

--- src/add_to_JSON.py
+++ src/add_to_JSON.py
@@ -1,4 +1,4 @@
-# Python script to update JSON 
+# Python script to update JSON
 import json
 import box
 import yaml
@@ -7,18 +7,19 @@ from pathlib import Path
 # Import config vars
 with open('config/config.yml', 'r', encoding='utf8') as ymlfile:
     cfg = box.Box(yaml.safe_load(ymlfile))
- 
+
 # function to add to JSON
 def write_json(new_data, filename=cfg.JSON_FILE_NAME):
-    with open(filename,'r+') as file:
-        # load existing data into a dict.
-        file_data = json.load(file)
-        # Join new_data with file_data inside chosen experiment
-        new_data.pop('path', None)
-             
-        file_data[cfg.JSON_EXPERIMENT_NAME].append(new_data)
-        # Sets file's current position at offset.
-        file.seek(0)
-        # convert back to json.
+    if Path(filename).exists():
+        with open(filename) as file:
+            file_data = json.load(file)
+    else:
+        file_data = {}
+
+    # Join new_data with file_data inside chosen experiment
+    new_data.pop('path', None)
+    file_data.setdefault(cfg.JSON_EXPERIMENT_NAME, []).append(new_data)
+
+    with open(filename, "w") as file:
         json.dump(file_data, file, indent = 4)
- 
+
