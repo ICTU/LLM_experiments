28c0c038184469485ab26d2e3318c251377084c6
JellevanE <73659687+JellevanE@users.noreply.github.com>
2024-02-29 10:06:17 +0100

    small adjustments (#9)
    * small improvments
    
    * added chat completion models to llm
    
    * error fixes and configurations for chat-model
    
    * added basic weighting function
    
    * added max_tokens function to llm.py, made separate file for skip dir+file
    
    * moved tokens_weighting to src and added few-shot setup
    
    ---------
    
    Co-authored-by: Jelle van Elburg <jelle.vanelburg@ictu.nl>

---
+++ src/tokens_weighting.py
@@ -0,0 +1,25 @@
+import os
+import math
+from pathlib import Path
+
+from src.skip_dir import skip_dir, skip_file
+
+
+def number_of_files(dir_path):
+    """Count number of files in a directory"""
+    count = 0
+    for path in os.scandir(dir_path):
+        if path.is_file() and not skip_file(Path(path)):
+            count += 1
+        if path.is_dir() and not skip_dir(Path(path)):
+            count += number_of_files(path)
+
+        else: continue
+    return count
+
+
+def max_num_tokens(path, base_max_tokens):
+    """Calculate the max numer of tokens usable in the output, takes base tokens and number of files as int input"""
+    number_of_files_in_dir = number_of_files(path)
+    max_num_tokens = math.ceil(base_max_tokens + math.sqrt(number_of_files_in_dir * 25))
+    return max_num_tokens
