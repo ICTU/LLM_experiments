2efb0a2eccd98d01d0bc29d8a463e3dd2463a2f8
Jelle van Elburg <jellevanelburg@gmail.com>
2024-04-02 18:25:13 +0200

    many small and large adjustments to chain form and output

--- 4-user_stories/src/evaluate.py
+++ 4-user_stories/src/evaluate.py
@@ -1,31 +1,69 @@
-from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.prompts import ChatPromptTemplate, PromptTemplate
 from src.llm import create_llm
 
 
-def evaluate_user_stories(input:list, fo_summary:str, use_case:str) -> list:
-    llm = create_llm()
-    prompt = evaluate_prompt(input=input, fo_summary=fo_summary, use_case=use_case)
-    return llm.invoke(prompt).content
+# def evaluate_user_stories(input:list, fo_summary:str, use_case:str) -> list:
+#     llm = create_llm()
+#     prompt = evaluate_prompt_po(input=input, fo_summary=fo_summary, use_case=use_case)
+#     return llm.invoke(prompt).content
 
 
-evaluate_template = ChatPromptTemplate.from_messages(
-    [
-        ("system", "You are a helpful and precise product owner."),
-        ("user", """You are given a summary of the functional design for an application (1), a specific use case for this application (2) and a list of user stories for this use case (3)
-        Please evaluate the list of user stories. Check if there are user stories that overlap or if there are stories that depend on other user stories. Adjust the list where necessary. 
+# evaluate_template_po = ChatPromptTemplate.from_messages(
+#     [
+#         ("system", "You are a helpful and precise product owner."),
+#         ("user", """You are given a summary of the functional design for an application (1), a specific use case for this application (2) and a list of user stories for this use case (3).
+#         Please evaluate the list of user stories. Check if there are user stories that overlap or if there are stories that depend on other user stories. For each user story and its acceptance criteria consider their pros and cons. Be very strict.
         
-        1. Functional design: ```{fo_summary}```
+#         1. Functional design: ```{fo_summary}```
+        
+#         2. Use case: ```{use_case}``
+         
+#         3. List of user stories: ```{user_stories_list}```
+         
+#         Comments:"""),
+#     ]
+# )
+
+
+# def evaluate_prompt_po(input, fo_summary, use_case):
+#     return evaluate_template_po.format_messages(user_stories_list=input, fo_summary=fo_summary, use_case=use_case)
+
+
+# evaluate_template_dev = ChatPromptTemplate.from_messages(
+#     [
+#         ("system", "You are a software developer working on an application."),
+#         ("user", """You are given a summary of the functional design for an application (1), a specific use case for this application (2) and a list of user stories for this use case (3).
+#         Please evaluate the list of user stories. Provide feedback on whether they are formulated clearly enough to work with as a developer.
+        
+#         1. Functional design: ```{fo_summary}```
+        
+#         2. Use case: ```{use_case}``
+         
+#         3. List of user stories: ```{user_stories_list}```
+         
+#         Comments:"""),
+#     ]
+# )
+
+def evaluate_prompt_po():
+    return PromptTemplate(input_variables=['functional_design', 'use_case', 'user_stories_list'], template=evaluate_template_po)
+
+evaluate_template_po = """You are a helpful and precise product owner. You are given a summary of the functional design for an application (1), a specific use case for this application (2) and a list of user stories for this use case (3).
+        Please evaluate the list of user stories. Check if there are user stories that overlap or if there are stories that depend on other user stories. For each user story and its acceptance criteria consider their pros and cons, initial effort needed, implementation difficulty, and potential challenges. 
+        
+        1. Functional design: ```{functional_design}```
         
         2. Use case: ```{use_case}``
          
-        3. List of user stories: ```{user_stories}```
-        
-        Return ONLY the adjusted list of user stories.
+        3. List of user stories: ```{user_stories_list}```
          
-        List:"""),
-    ]
-)
+        Comments:"""
 
+def evaluate_prompt_dev():
+    return PromptTemplate(input_variables=['po_comments'], template=evaluate_template_dev)
 
-def evaluate_prompt(input, fo_summary, use_case):
-    return evaluate_template.format_messages(user_stories=input, fo_summary=fo_summary, use_case=use_case)
+evaluate_template_dev = """You are a software developer working on an application. Add to the work of the product owner with your own evaluations. Provide feedback on whether the user stories are formulated clearly enough to work with as a developer. Return both the comments of the PO and your own thoughts.
+        
+        Comments product owener: {po_comments}
+         
+        Comments:"""
--- 4-user_stories/user_stories_main.py
+++ 4-user_stories/user_stories_main.py
@@ -6,21 +6,23 @@ from langchain.globals import set_llm_cache
 from langchain.cache import SQLiteCache
 
 from src.summarize_docs import summarize_docs_simple, write_to_file
-from sequence_chain import generate_user_stories
+from sequence_chain import generate_user_stories, evaluate_user_stories
 from docs.use_cases import use_cases_inkoop
+from src.add_to_JSON import write_json
 
 
 class User_stories(TypedDict):
      
     fo_doc:Path
     use_cases:dict
-    user_stories:list[User_story]
+    user_stories:dict
+    comments:str
      
 
 class User_story(TypedDict):
      
     use_case_key:str
-    title:str
+    description:str
     user_story:str
     acceptance_criteria:list
     open_issues:list
@@ -29,11 +31,17 @@ class User_story(TypedDict):
 def user_stories_main(fo_doc_path:Path, use_cases:dict, no_stories:int, results_path):
     """Uses a design document to generate user stories for a use cases"""
     fo_summary_text = summarize_docs_simple(fo_doc_path=fo_doc_path)
-    user_stories_dict = {}
+    
+
     for key, value in use_cases.items():
+        user_stories_dict = User_stories(fo_doc=fo_doc_path, use_cases=use_cases, user_stories={}, comments="")
         user_stories = generate_user_stories(fo_summary=fo_summary_text, use_case=value, no_stories=no_stories)
-        user_stories_dict.update({key: user_stories})
-    return write_to_file(user_stories_dict, results_path)
+        user_stories_dict['user_stories'] = user_stories
+        comments = evaluate_user_stories(fo_summary=fo_summary_text, use_case=value, user_stories_list=user_stories)
+        user_stories_dict['comments'] = comments
+        write_json(user_stories_dict, results_path)
+
+    return print(f"completed user stories and feedback, printed to {results_path}")
 
 fo_doc_path = "docs/Globaal-Functioneel-Ontwerp InkoopDB.docx"
 
@@ -42,5 +50,5 @@ if __name__ == "__main__":
         #doc_path = Path(sys.argv[1]).resolve(strict=True)
         doc_path = "docs/Globaal-Functioneel-Ontwerp InkoopDB.docx"
         use_cases = use_cases_inkoop
-        results_path = "results.py"
+        results_path = "results.json"
         user_stories_main(fo_doc_path=doc_path, use_cases=use_cases_inkoop, no_stories=5, results_path=results_path)
