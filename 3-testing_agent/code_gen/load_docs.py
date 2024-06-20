8887adf130d6b70d62de62a68ca0d36517914db9
JellevanE <73659687+JellevanE@users.noreply.github.com>
2024-06-20 14:46:52 +0200

    code gen: unit testing agent  (#33)
    * added output parser to order output
    
    * added seperate evaluation roles and translation to dutch
    
    * minor adjustments user stories test
    
    * started graph agent test
    
    * intermediate steps
    
    * added memory and search to agent graph
    
    * added human in the loop check
    
    * experimenting with code generation agent
    
    * file for agent
    
    * generate unit test for dir added
    
    * move file testing agent
    
    * generated unit tests using testing_prompts.py
    
    * last addition unit test agent

--- 3-testing_agent/code_gen/load_docs.py
+++ 3-testing_agent/code_gen/load_docs.py
@@ -1,17 +1,40 @@
-from bs4 import BeautifulSoup as Soup
-from langchain_community.document_loaders.recursive_url_loader import RecursiveUrlLoader
-from llm import code_gen_chain
+import os
+from skip_dir import skip_dir, skip_file
+from pathlib import Path
 
-# LCEL docs
-url = "https://langchain-ai.github.io/langgraph/concepts/"
-loader = RecursiveUrlLoader(
-    url=url, max_depth=20, extractor=lambda x: Soup(x, "html.parser").text
-)
-docs = loader.load()
 
-# Sort the list based on the URLs and get the text
-d_sorted = sorted(docs, key=lambda x: x.metadata["source"])
-d_reversed = list(reversed(d_sorted))
-concatenated_content = "\n\n\n --- \n\n\n".join(
-    [doc.page_content for doc in d_reversed]
-)
+def get_dir_files(path:Path) -> list[Path]:
+    file_paths = []
+    for root, dirs, files in os.walk(path):
+        # Filter out skipped directories
+        dirs[:] = [d for d in dirs if not skip_dir(Path(root) / d)]
+        
+        for file in files:
+            file_path = Path(root) / file
+            if not skip_file(file_path):
+                    file_paths.append(file_path)
+    return file_paths
+
+def get_dir_content(path:Path) -> dict:
+    file_content = {}
+    for file_path in get_dir_files(path):
+        try:
+            with open(file_path, 'r', encoding='utf-8') as f:
+                file_content[file_path] = f.read()
+        except UnicodeDecodeError:
+            try:
+                with open(file_path, 'r', encoding='latin-1') as f:
+                    file_content[file_path] = f.read()
+            except UnicodeDecodeError:
+                print(f"Skipping file: {file_path} (unsupported encoding)")
+    return file_content
+
+
+
+#load files
+concatenated_content = get_dir_content("C:/Users/jeelb/OneDrive - Stichting ICTU/Documenten/3. Code genAI/metrics/LLM_experiments/1-summarization/src/hash_register.py")
+
+functionality = "extending the hash register unit test"
+
+with open("C:/Users/jeelb/OneDrive - Stichting ICTU/Documenten/3. Code genAI/metrics/LLM_experiments/1-summarization/tests/test_hash_register.py", 'r', encoding='utf-8') as f:
+    unit_test = f.read()
